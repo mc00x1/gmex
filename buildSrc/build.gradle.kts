026ee73f138c1d0f6edb24ecb477c94682160eb3
Mikhail Chalabine <mc00x1@gmail.com>
2019-03-08 10:26:45 +0100

    Removed alrternative Kotlin DSL activation previously commented out

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,20 +1,13 @@
-import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 description = "BuildSrc Module"
 
 plugins {
     `kotlin-dsl`
-//    id("org.gradle.kotlin.kotlin-dsl").version("1.2.5")
-//    kotlin("jvm").version("1.3.20")
 }
-
-//val kotlinVersion = getKotlinPluginVersion()
 
 repositories {
     gradlePluginPortal()
-//    maven("https://repo.gradle.org/gradle/libs-releases-local/")
-//    maven("https://dl.bintray.com/kotlin/kotlinx/")
     maven("http://nexus/content/repositories/snapshots/")
     maven("http://nexus/content/repositories/releases/")
     maven("http://nexus/content/groups/public")
@@ -22,10 +15,6 @@ repositories {
 }
 
 dependencies {
-//    implementation(kotlin("stdlib", kotlinVersion))
-//    implementation(kotlin("reflect", kotlinVersion))
-//    compile(kotlin("gradle-plugin", kotlinVersion))
-//    compile("org.gradle:gradle-kotlin-dsl:1.1.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.0")
 }
